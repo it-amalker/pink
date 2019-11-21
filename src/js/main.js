dc49a425630972d775266a9d0985b4654f70edcf
amalker <it-amalker@yandex.ru>
2019-11-21 12:45:07 +0300

    added menu open/close script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
-$(function () {
+let mainNav = document.querySelector('.main-nav');
+let openButton = document.querySelector('.logo__button-nav-open');
+let closeButton = document.querySelector('.logo__button-nav-close');
 
 
-});
+
+openButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mainNav.classList.add('main-nav--active');
+  openButton.classList.remove('logo__button-nav-open--active');
+  closeButton.classList.add('logo__button-nav-close--active');
+})
+
+closeButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mainNav.classList.remove('main-nav--active');
+  openButton.classList.add('logo__button-nav-open--active');
+  closeButton.classList.remove('logo__button-nav-close--active');
+})
